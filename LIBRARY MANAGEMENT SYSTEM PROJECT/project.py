861d9ba94cb7806e7e47813ef1251e79d1b75f40
grooti76 <shivkumarjha76@gmail.com>
2022-09-08 22:39:19 +0530

    Create project.py

---
+++ LIBRARY MANAGEMENT SYSTEM PROJECT/project.py
@@ -0,0 +1,60 @@
+# Library management System
+class Library:
+    def __init__(self, listOfBooks):
+        self.books = listOfBooks
+
+    def displayAvailableBooks(self):
+        print("Books available in this library are: ")
+        for book in self.books:
+            print(":- " + book)
+
+    def borrowBook(self, bookName):
+        if bookName in self.books:
+            print(
+                f"You have been issued {bookName}. Please return it within 30 days")
+            self.books.remove(bookName)
+            return True
+        else:
+            print("Sorry, Please wait until the book is available")
+            return False
+
+    def returnBook(self, bookName):
+        self.books.append(bookName)
+        print("Thanks for returning this book! Hope you enjoyed reading it. Have a great day ahead!")
+
+
+class Student:
+    def requestBook(self):
+        self.book = input("Enter the name of the book you want to borrow: ")
+        return self.book
+
+    def returnBook(self):
+        self.book = input("Enter the name of the book you want to return: ")
+        return self.book
+
+
+if __name__ == "__main__":
+    centraLibrary = Library(["Python Programming", "DSA", "CG", "OS", "DBMS"])
+    student = Student()
+    while(True):
+        welcomeMsg = '''\n ====== Welcome to Central Library ======
+        Please choose an option:
+        1. List all the books
+        2. Request a book
+        3. Add/Return a book
+        4. Exit the Library
+        '''
+        print(welcomeMsg)
+        a = int(input("Enter a choice: "))
+        if a == 1:
+            centraLibrary.displayAvailableBooks()
+        elif a == 2:
+            centraLibrary.borrowBook(student.requestBook())
+        elif a == 3:
+            centraLibrary.returnBook(student.returnBook())
+        elif a == 4:
+            print(
+                "Thanks for Coming. Hope you Learn something new. Have a great day ahead!")
+            exit()
+        else:
+            print("Wrong Choice!, Please Select options given above")
