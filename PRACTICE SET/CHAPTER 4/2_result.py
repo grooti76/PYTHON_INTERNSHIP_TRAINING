3b9af9b375d8647c92a81bc190a477f98219ab51
grooti76 <shivkumarjha76@gmail.com>
2022-09-10 11:12:50 +0530

    chapter 4

---
+++ PRACTICE SET/CHAPTER 4/2_result.py
@@ -0,0 +1,6 @@
+marks = []
+for i in range(6):
+    k = i+1
+    marks.append(input(f"Enter {k} marks:- "))
+marks.sort()
+print(marks)
---
+++ PRACTICE SET/CHAPTER 4/4_sumlist.py
@@ -0,0 +1,6 @@
+num = []
+for i in range(4):
+    k = i+1
+    num.append(int(input(f"Enter {k} number:- ")))
+a = sum(num)
+print("The Sum of the given numbers is", a)
